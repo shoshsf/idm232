7f52b20219ad9ce83db214aee5caad67f44a79fa
shoshsf <72558473+shoshsf@users.noreply.github.com>
2022-10-06 19:08:05 -0400

    Assignment 01

---
+++ Final/index.php
@@ -0,0 +1,26 @@
+<?php include 'test.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Final</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  
+
+  <?php
+        $title ="Assignment - 01";
+    ?>
+
+
+  <body>
+     <h1> <?php echo $title?></h1>
+  </body>
+
+
+</html>
