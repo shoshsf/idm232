891c265315c83435b7ceeb9f17a1c30cf9c4330b
shoshsf <72558473+shoshsf@users.noreply.github.com>
2022-10-23 04:19:25 -0400

    Last Update 10/23 4:19
    - added text Header Logo
    - Added all fonts
    - Working on hover and button animation for Top pick

--- Final/Alpha/index.php
+++ Final/Alpha/index.php
@@ -2,11 +2,11 @@
 
 <!--Body Here-->
 <div class="topSection">
-    <div class="logoHeader">
-         <img src="imgs/logo2.png" id="logo" class="center" alt="">
-        <!-- <h1>
+    <div class="logoHeader responsive">
+         <!-- <img src="imgs/logo3.png" id="logo" class="center " alt=""> -->
+         <h1 id="logo">
             Kazi's Kitchen
-        </h1> -->
+        </h1>
     </div>
 
     <div class="nav">
@@ -22,10 +22,32 @@
     <div id="welcomeText">
         <p> 
             Welcome to my kitchen! I know how hard it is to find delicious recipes that you absolutly love. That’s why I am here to share my collection of recipes with you. The collection consists of a variety of healthy and delicious dishs.
-            Hope you find a recipe you like in my Kitchen.
+            <br>Hope you find a recipe you like in my Kitchen!
         </p>
     </div>
+    <hr class="hr_30">
+    <div>
+        <!-- search Bar -->
+    </div>
+      <!-- Top Pick Section-->
+      <div id="topPickSection">
+        <div id="display">
+        <p id="topPickHeader">Today's Top Pick <p>
+            <div class="container center">
+            <img src="imgs/f3_880x400.jpg" alt="Top Pick" class="image responsive" >
+            <div class="overlay">
+                <div class="slideText">
+                    <p class="text">Lorem ipsum </p>
+                    <p class="subtext">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <a class="button" href="#" target="_blank" > 
+                    View Recipe</a>
+                </div>
+            </div>
+            </div>
+        </div> 
+    <div>
 </div>
+
 <!--Body eNDSHere-->
 
 <?php include_once 'components/footer.php'; ?>
